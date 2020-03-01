b11a912486f3cdc9a5f9c7e81fce063851261f48
Neville <neville.code@gmail.com>
2020-03-01 15:50:52 +1300

    modified Main activity

--- app/src/main/java/com/ym/calculator/MainActivity.java
+++ app/src/main/java/com/ym/calculator/MainActivity.java
@@ -3,6 +3,7 @@ package com.ym.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i(TAG, "--onCreate--");
+    }
+
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "--onRestart--");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "--onResume--");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "--onPause--");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "--onStop--");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "--onDestroy--");
     }
 }
